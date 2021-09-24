e135c3c073b5ba1c4fc8af4e6d1aefe94aab4a8d
acmenes <acmenescompositions@gmail.com>
2021-09-24 12:00:51 -0400

    displays list of users on admin page

--- public/js/listusers.js
+++ public/js/listusers.js
@@ -1,14 +1,30 @@
 // const users = require('../init_data.json').data;
 
-const table = document.getElementById("user-table");
+// const table = document.getElementById("user-table");
 // const dataFromJSON = JSON.parse("../init_data.json")
 
-createTableFromJSON()
+let usersArray = []
 
-async function createTableFromJSON() {
+async function getData() {
     const res = await axios.get('./users')
     console.log(res.data)
+    usersArray.push(res.data)
+    populateTable()
     return res.data
 };
+
+function populateTable() {
+    for(let x = 0; x < usersArray[0].length; x++) {
+        $('#user-table').append(`<tr>
+                        <td>${usersArray[0][x].id}</td>
+                        <td>${usersArray[0][x].email}</td>
+                        <td>${usersArray[0][x].firstName}</td>
+                        <td>${usersArray[0][x].lastName}</td>
+                        <td>${usersArray[0][x].state}</td>
+                                </tr>`)
+    }
+};
+
+getData();
 
 
